59d7cdf2ebe3b98d8586531aea9701b83cbedc70
adedayojs <samfeolu@gmail.com>
2019-09-17 17:12:44 +0100

    Created Popular Starships Component

---
+++ src/components/PopularStarships/Popular.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <h1 class="popular-section-header">Popular {{title}}</h1>
+    <div class="card-container">
+      <div class="card-div">
+        <img class="card-image" src="{props.img[randomImage]" } />
+        <h3>{props.name}</h3>
+        <p>sample test</p>
+        <span>Read More</span>
+      </div>
+    </div>
+    <div class="read-more">Read More</div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Starship",
+  props: {
+    msg: String,
+    title: String,
+    character: Object
+  },
+  methods: {
+    randomImage(imgArr) {
+      randomImage = Math.floor(Math.random() * imgArr.length);
+      return imgArr[randomImage];
+    }
+  },
+  beforeMount() {
+    console.log(this.character);
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.jumbotron {
+  height: 50vh;
+  background: url("../../assets/hero-banner.jpg");
+}
+</style>
