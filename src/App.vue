679a20f6a67ab47fb9f9ca5f3a5f31b6bb419b14
adedayojs <samfeolu@gmail.com>
2019-09-18 09:46:14 +0100

    Implemented Planet Component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <Jumbotron />
     <Starships title="Starships" />
+    <Planets title="Planets" />
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
@@ -10,13 +11,15 @@
 import HelloWorld from "./components/HelloWorld.vue";
 import Jumbotron from "./components/Jumbotron/Jumbotron.vue";
 import Starships from "./components/PopularStarships/Popular.vue";
+import Planets from "./components/Planets/PopularPlanets.vue";
 
 export default {
   name: "app",
   components: {
     HelloWorld,
     Jumbotron,
-    Starships
+    Starships,
+    Planets
   },
   methods: {},
   data: function() {
