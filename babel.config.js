e1a8125fc441b25b7a7f1b0c66def4bfbaf753a9
adedayojs <samfeolu@gmail.com>
2019-09-19 23:00:02 +0100

    Resolved material desing to have a shorter import statement

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  presets: [
-    '@vue/app'
-  ]
-}
+  presets: ['@vue/app'],
+  resolve: {
+    alias: {
+      icons: path.resolve(__dirname, 'node_modules/vue-material-design-icons')
+    },
+    extensions: ['.vue']
+  }
+};
