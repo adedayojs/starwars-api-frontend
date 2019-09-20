a53314a6957f07cce3aaff11d87dac9fe6a3e06b
adedayojs <samfeolu@gmail.com>
2019-09-20 18:52:55 +0100

    Removed Resolve because it was giving issues with building

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   presets: ['@vue/app'],
-  resolve: {
-    alias: {
-      icons: path.resolve(__dirname, 'node_modules/vue-material-design-icons')
-    },
-    extensions: ['.vue']
-  }
+
 };
