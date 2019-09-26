1c4778c9059acfc811ff7a5868347158f101360c
adedayojs <samfeolu@gmail.com>
2019-09-26 10:13:41 +0100

    Added styling

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -6,13 +6,13 @@
 
     <div class="logo">
       <img src="/img/logo.png" />
-      <h3 style="font-weight:900;font-size:2em;margin-left:0.5em">Directory</h3>
+      <h3>Directory</h3>
     </div>
     <p>
       Find Your Favourite Character, Films, Species,
       <br />Starships and Planets
     </p>
-    <form style="display:flex">
+    <form style="display:flex; ">
       <div class="input">
         <input type="text" placeholder="Search by Name" v-model="search" @keyup="searchHandler()" />
       </div>
@@ -49,7 +49,7 @@ export default {
 .jumbotron {
   height: 50vh;
   background: url("/img/hero-banner.jpg");
-  font-size: 1em;
+  font-size: 2em;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -60,15 +60,28 @@ export default {
   display: flex;
   align-items: center;
   color: #fff;
+  /* font-size: 2em; */
   border-bottom: 3px solid white;
 }
-img {
-  width: 8em;
-  height: 3em;
+.logo img{
+  width: 5em;
+  height: 2em;
+}
+h3 {
+  font-weight: 900;
+  margin-left: 0.5em;
+  margin-bottom: 0px;
+  position: relative;
+  top: -0.5em;
+}
+.topmost-logo img {
+  width: 5em;
+  height: 2em;
 }
 .topmost-logo {
-  align-self: flex-start;
-  margin: -5em auto 2em 1em;
+  position: absolute;
+  top: 2em;
+  left: 2em;
 }
 .search {
   height: 100%;
@@ -83,6 +96,7 @@ select {
   display: flex;
   padding: 0.5em;
   background: #d8d8d8;
+  width:20em
 }
 
 input {
